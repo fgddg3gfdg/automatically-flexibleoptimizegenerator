ae75f4930743e8c81b76b4fbd26a3d88de5e3c84
fgddg3gfdg <gs1yobrodog@hotmail.com>
2024-05-12 02:19:00 +0700

    First commit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-function minPathSum(grid) {
-  const rows = grid.length;
-  const cols = grid[0].length;
-  for (let i = 1; i < rows; i++) {
-    grid[i][0] += grid[i - 1][0];
-  }
-  for (let j = 1; j < cols; j++) {
-    grid[0][j] += grid[0][j - 1];
-  }
-  for (let i = 1; i < rows; i++) {
-    for (let j = 1; j < cols; j++) {
-      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+function combinationSum3(k, n) {
+  const result = [];
+  backtrack([], 1, k, n);
+  return result;
+  function backtrack(combination, start, k, n) {
+    if (n === 0 && k === 0) {
+      result.push([...combination]);
+      return;
+    }
+    if (n < 0 || k === 0) return;
+    for (let i = start; i <= 9; i++) {
+      combination.push(i);
+      backtrack(combination, i + 1, k - 1, n - i);
+      combination.pop();
     }
   }
-  return grid[rows - 1][cols - 1];
 }
