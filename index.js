54485ab455e38cdc7d4ab3a5c6247c074d3a5c4f
fgddg3gfdg <gs1yobrodog@hotmail.com>
2024-05-15 20:07:14 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-function detectCycle(head) {
-  let slow = head;
-  let fast = head;
-  let hasCycle = false;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (slow === fast) {
-      hasCycle = true;
-      break;
-    }
-  }
-  if (!hasCycle) return null;
-  slow = head;
-  while (slow !== fast) {
-    slow = slow.next;
-    fast = fast.next;
-  }
-  return slow;
-}
+const numbers = [1, 5, 2, 8];
+const max = Math.max(...numbers);
+console.log(max); // 8
