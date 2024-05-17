67a09f037a81aa118c975623cc542ea878aa4f49
fgddg3gfdg <gs1yobrodog@hotmail.com>
2024-05-17 14:14:29 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-function countPrimes(n) {
-  const isPrime = new Array(n).fill(true);
-  isPrime[0] = false;
-  isPrime[1] = false;
-  for (let i = 2; i * i < n; i++) {
-    if (isPrime[i]) {
-      for (let j = i * i; j < n; j += i) {
-        isPrime[j] = false;
+function numIslands(grid) {
+  if (!grid || grid.length === 0) return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  let islands = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        islands++;
+        dfs(grid, i, j);
       }
     }
   }
-  let count = 0;
-  for (let i = 2; i < n; i++) {
-    if (isPrime[i]) count++;
-  }
-  return count;
+  return islands;
+}
+function dfs(grid, i, j) {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
 }
